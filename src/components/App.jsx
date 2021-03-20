98b735440bd8368d26c8da02134cd73d1f84d1e9
sudachi0114 <jnevariste@gmail.com>
2021-03-20 18:31:37 +0900

    検索キーワードをフォームから指定できるように変更
    React って公式のヘルプがすぐ出てきて助かる
    https://ja.reactjs.org/docs/forms.html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,35 @@ import axios from 'axios';
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.getQiitaPosts = this.getQiitaPosts.bind(this);
+
     this.state = {
-      query: 'React'  // 検索したいワード
-    }
+      query: 'React', // 検索したいワード
+      keyword: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState( {keyword: event.target.value} );
+    console.log("state.keyword" + this.state.keyword)
+  }
+
+  handleSubmit(event) {
+    alert('検索キーワードが送信されました:' + this.state.keyword);
+    event.preventDefault();
   }
 
   getQiitaPosts() {
+    console.log(this.state.keyword)
     axios.get('https://qiita.com/api/v2/items', {
         params: {
           "page": "1",
           "per_page": "20",
-          "query": this.state.query,
+          "query": this.state.keyword, // this.state.query,
         }
     }).then( response => {
       const data = response.data[0];
@@ -37,12 +54,20 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="app-title">Qiita API Service</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            検索キーワード
+            <input type="text" name="検索キーワード" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="検索" onClick={() => this.getQiitaPosts()} /> 
+        </form>
         <p> title: { this.state.title } </p>
         <p> URL: <a target="__blank" href={this.state.url}> { this.state.url } </a></p>
-        <input type="button" value="検索" onClick={() => this.getQiitaPosts()} />
       </div>
     )
   }
 }
+
+//  onClick={(keyword) => this.getQiitaPosts(keyword)} 
 
 export default App;
