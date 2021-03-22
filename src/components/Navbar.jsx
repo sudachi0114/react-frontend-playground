72dbda11b069d439ce9d556453e8dde08e49a796
sudachi0114 <jnevariste@gmail.com>
2021-03-22 19:08:51 +0900

    apply Bootstrap decoration. ref: https://getbootstrap.jp/docs/5.0/components/navbar/

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom';
 class Navbar extends React.Component {
   render() {
     return(
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/qs">QiitaService</Link>
-      </div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">Navbar</a>
+          <Link to="/">Home</Link>
+          <Link to="/qs">QiitaService</Link>
+        </div>
+      </nav>
     )
   }
 }
