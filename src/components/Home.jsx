411057b274967c34155041ecb139c01ea7b1079c
sudachi0114 <jnevariste@gmail.com>
2021-03-22 19:03:18 +0900

    もう少し詳しいページにした

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-          Home
+        <h1> React Frontend Playground </h1>
+
+        <p> 
+          Created by: <a href="https://github.com/sudachi0114"> sudachi </a>
+        </p>
+
+        <p>
+          Repository is <a href="https://github.com/sudachi0114/react-frontend-playground">Here</a>
+        </p>
+
+
+        <Link to="/">Home</Link>
+        <Link to="/qs">QiitaService</Link>
+
       </div>
     )
   }
